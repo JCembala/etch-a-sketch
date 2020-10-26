8d35f0cf139bfb8d3abf4c15b619a9a74b8e855a
JCembala <cembala.jakub@gmail.com>
2020-10-26 06:09:43 +0100

    refactored code which was creating pad to allow creating any size

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,9 +1,15 @@
-let squares = new Array(16 * 16);
 const container = document.querySelector('.pad');
 
-for (let i = 0; i < squares.length; i++) {
-    squares[i] = document.createElement('div');
-    squares[i].innerText = i + 1;
-    squares[i].classList.add('pad__square')
-    container.appendChild(squares[i]);
+function createPad(size) {
+
+    let squares = new Array(size * size);
+
+    for (let i = 0; i < squares.length; i++) {
+        squares[i] = document.createElement('div');
+        squares[i].innerText = i + 1;
+        squares[i].classList.add('pad__square')
+        container.appendChild(squares[i]);
+    }
 }
+
+createPad(16);
